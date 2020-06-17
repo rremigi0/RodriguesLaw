3afb8fe4d04b1d641adacb00a20b48967812b37e
Raphael Remigio Rodrigues <rremigio@rodrigues.cnt.br>
2020-06-17 15:15:14 -0300

    make it better

--- routes/processo.js
+++ routes/processo.js
@@ -37,7 +37,7 @@ router.get("/view_arquivados", eAdmin, (req, res) => {Processo.find( { Status:'A
 
 // Visualizar os Processos Pendentes:
 
-router.get("/view_pendentes", eAdmin, (req, res) => {Processo.find().sort({Atuacao:1}).then((processos) => {
+router.get("/view_pendentes", eAdmin, (req, res) => {Processo.find({ Status:'' }).sort({Atuacao:1}).then((processos) => {
     res.render("Processos/view_pendentes", {processos: processos})}).catch((err) => {req.flash("error_msg", "houve um erro ao listar os processos")
     res.redirect("/processo/main")})})
 
