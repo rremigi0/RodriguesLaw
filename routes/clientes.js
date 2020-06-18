d7a532a7a5532146bbebe278cb87dc3a42f46a62
Raphael Remigio Rodrigues <rremigio@rodrigues.cnt.br>
2020-06-17 22:32:39 -0300

    make it better

--- routes/clientes.js
+++ routes/clientes.js
@@ -8,7 +8,14 @@ const {eAdmin2} = require("../helpers/eAdmin2")
 
 //Rota de Clientes:
 
-router.get('/main', eAdmin, (req, res) => {res.render("Clientes/main")})
+router.get('/main', eAdmin, (req, res) => {
+    Cliente.find().then((clientes) => {
+        res.render("Clientes/main", {clientes: clientes})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar os clientes")
+        res.redirect("/cliente/main")
+    })
+})   
 
 // Rota Adicionar Novo Cliente:
 
--- routes/processo.js
+++ routes/processo.js
@@ -35,6 +35,14 @@ router.get("/view_arquivados", eAdmin, (req, res) => {Processo.find( { Status:'A
     res.render("Processos/view_arquivados", {processos: processos})}).catch((err) => {req.flash("error_msg", "houve um erro ao listar os processos")
     res.redirect("/processo/main")})})
 
+// Visualizar os Processos Pendentes:
+
+router.get("/view_pendentes", eAdmin, (req, res) => {Processo.find().sort({Cliente:1}).then((processos) => {
+    res.render("Processos/view_pendentes", {processos: processos})}).catch((err) => {req.flash("error_msg", "houve um erro ao listar os processos")
+    res.redirect("/processo/main")})})
+
+
+
 // Deletar Processos
 
 router.get("/deletar/:id", eAdmin2, (req, res) => {Processo.remove({_id: req.params.id}).then(() => {req.flash("success_msg", "Processo deletado com sucesso")
